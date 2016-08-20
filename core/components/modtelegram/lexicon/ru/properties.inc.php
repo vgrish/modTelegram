c27b403f9b13bea804e88b4d7f116de9416c6f1f
vovanblya <vovanblya@mail.ru>
2016-08-20 17:30:17 +0300

    1.0.9-beta - (20.08.2016) ============== - Improvements "lexicon"

--- core/components/modtelegram/lexicon/ru/properties.inc.php
+++ core/components/modtelegram/lexicon/ru/properties.inc.php
@@ -4,4 +4,6 @@ $_lang['modtelegram_prop_actions'] = 'Список доступных дейст
 $_lang['modtelegram_prop_helper'] = 'Параметры помощника';
 $_lang['modtelegram_prop_frontendCss'] = 'Файл с css стилями для подключения на фронтенд.';
 $_lang['modtelegram_prop_frontendJs'] = 'Файл с javascript для подключения на фронтенде.';
+$_lang['modtelegram_prop_frontendLexicon'] = 'Лексиконы для подключения на фронтенде.';
 $_lang['modtelegram_prop_actionUrl'] = 'Коннектор для обработки ajax запросов.';
+
